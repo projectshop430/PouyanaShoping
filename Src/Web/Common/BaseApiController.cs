70507fc7e046c6087a5ccfeeba478584611c612f
projectshop430 <projectshop430@gmail.com>
2023-11-29 11:25:18 +0330

    1-2-Relations Layers

---
+++ Src/Web/Common/BaseApiController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+
+namespace Web.Common
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BaseApiController : ControllerBase
+    {
+      
+    }
+}
